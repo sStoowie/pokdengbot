65ce23cce5db248dbe2de61afb7f1f672b932faf
chanmathaearth <107950430+chanmathaearth@users.noreply.github.com>
2022-12-10 17:53:32 +0700

    Update pokdengbot.py

--- pokdengbot.py
+++ pokdengbot.py
@@ -1,39 +1,60 @@
 import discord
 import random
-    
 bot = discord.Bot()
-rank = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-add = ["♠️", "♥️", "♣", "♦️"]
-card = random.choice(add)+random.choice(rank)
-mycard1 = random.choice(rank)
-mycard2 = random.choice(rank)
-dealercard = random.choice(rank)
-dealercard = random.choice(rank)
-score = 0
-if mycard1 in ["K", "Q", "J"]:
-    score += 10
-elif mycard1 == "A":
-    score += 1
-else:
-    score += int(mycard1)
-if mycard2 in ["K", "Q", "J"]:
-    score += 10
-elif mycard2 == "A":
-    score += 1
-else:
-    score += int(mycard2)
+
+def calculate_score(cards):
+    sumscore = 0
+    comp = ["K", "Q", "J"]
+    if type(cards[0]) == int:
+        sumscore += cards[0]
+    if type(cards[1]) == int:
+        sumscore += cards[1]
+    if cards[0] == "A":
+        sumscore += 1
+    if  cards[1] == "A":
+        sumscore += 1
+    if cards[0] in comp:
+        sumscore += 10
+    if cards[1] in comp:
+        sumscore += 10
+    if sumscore >= 10:
+        sumscore -= 10
+    return sumscore
+def deal_card():
+    """Returns a random card"""
+    cards = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
+    return random.choice(cards)
+
+def compare(aaa,bbb):
+    if aaa > bbb:
+        return "Player on Fire"
+    else:
+        return "Dealer As always"
+
+add = ["♠️", "♣️", "♥️", "♣", "♦️"]
+
+dealer_card = []
+player_card = []
+dealer_score = 0
+player_score = 0
+for _ in range(2):
+    player_card.append(deal_card())
+    dealer_card.append(deal_card())
+dealer_score = calculate_score(dealer_card)
+player_score = calculate_score(player_card)
 @bot.event
-    
 async def on_ready():
     print("I am online!!")
-@bot.slash_command(name = "hand")
-    
+@bot.slash_command(name = "hand", description="จั่วไพ่")
+
 async def main(ctx):
-    embed = discord.Embed(title="POKDENG", description="เริ่มตาของคุณ!", color=0xe6337a)
-    embed.add_field(name="Your hand", value=random.choice(add)+mycard1+"  "+random.choice(add)+mycard2, inline=True)
-    embed.add_field(name="Dealer hand", value="??", inline=True)
-    embed.add_field(name="Your score", value=score, inline=False)
+    embed = discord.Embed(title="POKDENG", description="เริ่มตาของคุณ!!!", color=0xe6337a)
+    embed.add_field(name="Your hand", value=random.choice(add)+str(player_card[0])+"  "+random.choice(add)+str(player_card[1]), inline=True)
+    embed.add_field(name="Dealer hand", value=random.choice(add)+str(dealer_card[0])+"  "+random.choice(add)+str(dealer_card[1]), inline=False)
+    embed.add_field(name="Your Score", value=calculate_score(player_card), inline=True)
+    embed.add_field(name="Dealer Score", value=calculate_score(dealer_card), inline=True)
     embed.set_thumbnail(url='https://i.imgur.com/tVbh1Ke.png')
-    await ctx.respond(embed=embed)
-bot.run('MTA0MTI1MjE0MTM3NDkwMjM4Mg.GxipOy.b_kWP6llWII5UHAYdjk3YhBi_-TUdD5ui5v94M')
+    embed.add_field(name="The Winner Is", value=compare(calculate_score(player_card), calculate_score(dealer_card)), inline=False)
+    await ctx.respond("มาเล่นกันเถอะ!!!!", embed=embed)
+bot.run('MTA0MTI1MjE0MTM3NDkwMjM4Mg.GWV-Wr.Nsw3we4p9g0yQU_KvsWbiINZaEsc8egfzt6c88')
 
